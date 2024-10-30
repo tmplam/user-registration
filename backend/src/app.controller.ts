9a2eaf42b93ddcfb6ec20372ffd81a48d5eaddad
tmplam <phulam.dev@gmail.com>
2024-10-30 08:00:34 +0700

    add check endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getServerStatus() {
+    return 'Server ran successfully.';
+  }
 }
